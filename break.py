8f6ded768974c63d4bc84daa462d5b21fe59f886
Kevin Ndung'u <kevgathuku@gmail.com>
2014-05-03 16:58:21 +0300

    Modufy break.py to run after every 2 hours

--- break.py
+++ break.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python
 
 import webbrowser, time
+
+#URL to be opened
 url = "http://youtu.be/u0fk6syQ7iY?t=26s"
+print("Program starts at " + time.ctime())
+
+#Run the script three times, after every 2 hours
 for i in range(3):
-	time.sleep(5)
+	time.sleep(2*60*60)
 	webbrowser.open(url)
