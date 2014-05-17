94b3404a7b52ff923e2d63f26270f70c5ecdfb36
Kevin Ndung'u <kevgathuku@gmail.com>
2014-05-17 03:01:58 +0300

    Add correct time, minor modifications

--- break.py
+++ break.py
@@ -2,10 +2,12 @@
 
 import webbrowser, time, urllib2, os
 
+print("Program starts at " + time.ctime())
+
 def internet_on():
-	"""
-	Detect internet connectivity and return True if an internet connection is present, and False otherwise
-	"""
+    """
+    Detect internet connectivity and return True if an internet connection is present, and False otherwise
+    """
     try:
         response=urllib2.urlopen('http://74.125.228.100',timeout=3)
         return True
@@ -13,24 +15,22 @@ def internet_on():
     return False
 
 #URL to be opened
-youtube = "http://youtu.be/u0fk6syQ7iY?t=26s"
-local_url = os.path.abspath(os.path.join(os.path.dirname(__file__), 'index.html'))
 
-print("Program starts at " + time.ctime())
+def take_a_break(result):
+    """
+    Opens a Youtube video or a local page after every 2 hours, depending on internet connectivity.
 
-def take_a_break():
-	"""
-	Opens a Youtube video or a specified page after every 2 hours, depending on internet connectivity.
+    If an internet connection is present, open a Youtube video specified in the URL parameter.\
+    If no internet connection is available, open a local html page.
+    """
+    youtube = "http://youtu.be/u0fk6syQ7iY?t=26s"
+    local_url = os.path.abspath(os.path.join(os.path.dirname(__file__), 'index.html'))
+    #Run the script three times, after every 2 hours,
+    time.sleep(2*60*60)
+    if result == True:
+        webbrowser.open(youtube)
+    else:
+        webbrowser.open(local_url)
 
-	If an internet connection is present, open a Youtube video specified in the URL parameter. If no internet connection is available, open a local html page.
-	"""
-	#Run the script three times, after every 2 hours,
-	for i in range(3):
-		time.sleep(5)
-		result = internet_on()
-		if result == True:
-			webbrowser.open(youtube)
-		else:
-			webbrowser.open(local_url)
-
-take_a_break()
+if __name__ == '__main__':
+    take_a_break(internet_on)
